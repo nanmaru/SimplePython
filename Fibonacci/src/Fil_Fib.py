2a7f6b04cf783960bfef1d7daa18e6831a9652ae
nanmaru <nguyen.tony29@gmail.com>
2016-07-18 11:03:37 -0600

    Base cases are handled. Need to do more tests to validate that this code
    is working as intended.

--- Fibonacci/src/Fil_Fib.py
+++ Fibonacci/src/Fil_Fib.py
@@ -1,14 +1,55 @@
 '''
-Created on Jun 23, 2016
-
-@author: Fil Gibarac
+Created on Jun 16, 2016
+ 
+@author: fil
 '''
-def fib(x):
-   
+def fib():
     F = [1,1]
-    for i in range(2,x+1):
-        y = F[i-1] +F[i-2]
-        F.append(y);
-    print(F);
-           
-print(fib(7))
+    user_input_x = int(input("Starting Index: "))
+    user_input_y = int(input("Ending Index: "))
+    if user_input_y == 1:
+        F = [1]
+    elif user_input_y < 1:
+        print("Invalid Range")
+        quit()
+    elif user_input_x < 0:
+        print("Invalid Range")
+        quit()   
+    elif user_input_x > user_input_y:
+        print("Invalid Range")
+        quit()
+    elif user_input_x == user_input_y:
+        print("Invalid Range")
+        quit()
+    else:
+        for i in range(2,user_input_y):
+            z = F[i-1]+F[i-2]
+            F.append(z);
+        #print(F)
+       
+        if user_input_x == 0:
+            x1 = 1
+        elif user_input_x == 1:
+            x1 = 0
+        else:
+            x1 = F[user_input_x - 2]
+       
+        if user_input_x == 0:
+            x2 = 0
+        elif user_input_x == 1:
+            x2 = 1
+        else:
+            x2 = F[user_input_x - 1]
+       
+        #print(x1, x2)
+    x3 = x1+x2
+    G = [x3,x3+x2]
+    #print(G)
+    #print(G)
+    for i in range(2,user_input_y-user_input_x+1):
+            w = G[i-1]+G[i-2]
+            G.append(w);
+    #print(G)
+    print("Your Fibonacci Sequence is: ")
+    print(G)
+fib()
